1c248a8a4a9e36b775cd941fa76337240ec59a3f
wudi <wudi2852987@163.com>
2016-01-18 15:54:47 +0800

    swich mongodb ip

--- user.js
+++ user.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 if (conifg.NODE_ENV == 'dev') {
     mongoose.connect('mongodb://passport:passport@192.168.112.94:27017/passport');
 } else {
-    mongoose.connect('mongodb://127.0.0.1:27017/passport');
+    mongoose.connect('mongodb://115.159.93.161:27017/passport');
 }
 
 
